095d8c997fc90e209153a59c380716a9b9be3465
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-02-18 19:22:38 +0000

    meta for iphone

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Family Chat</title>
         <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="icon" href="./favicon.ico" />
       </Head>
 
